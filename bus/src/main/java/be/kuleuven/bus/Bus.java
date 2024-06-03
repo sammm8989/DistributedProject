be25c47d4f2c147a231b24d65795cec62264e439
Jens <jens.coosemans@student.kuleuven.be>
2024-06-03 02:06:39 +0200

    small change for broker

--- bus/src/main/java/be/kuleuven/bus/Bus.java
+++ bus/src/main/java/be/kuleuven/bus/Bus.java
@@ -5,10 +5,7 @@ import java.time.LocalDateTime;
 public class Bus {
     protected Integer id;
     protected String type_to;
-    protected LocalDateTime to_festival;
     protected String type_from;
-    protected LocalDateTime from_festival;
-    protected BoardingLocation location;
     protected Float price;
     protected Boolean confirmed;
 
@@ -36,36 +33,12 @@ public class Bus {
         this.type_from = type_from;
     }
 
-    public LocalDateTime getTo_festival() {
-        return to_festival;
-    }
-
-    public void setTo_festival(LocalDateTime to_festival) {
-        this.to_festival = to_festival;
-    }
-
-    public LocalDateTime getFrom_festival() {
-        return from_festival;
-    }
-
     public Float getPrice() {
         return price;
     }
 
     public void setPrice(Float price) {
         this.price = price;
-    }
-
-    public void setFrom_festival(LocalDateTime from_festival) {
-        this.from_festival = from_festival;
-    }
-
-    public BoardingLocation getLocation() {
-        return location;
-    }
-
-    public void setLocation(BoardingLocation location) {
-        this.location = location;
     }
 
     public Boolean getConfirmed() {
